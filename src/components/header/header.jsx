71dc2b510cd36b677f4cdfb9199dbb1ffedb1ef0
Ashan Jameel <59074253+Ashan01@users.noreply.github.com>
2021-02-18 23:13:55 +0500

    commit

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,26 +1,13 @@
 import React from "react";
-// import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
-import { Navbar, Nav, Form, FormControl, Button } from "bootstrap";
+import Style from "./header.module.css";
 
 export default function Header() {
    return (
-      <div>
-         <Navbar bg="light" variant="light">
-            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-            <Nav className="mr-auto">
-               <Nav.Link href="#home">Home</Nav.Link>
-               <Nav.Link href="#features">Features</Nav.Link>
-               <Nav.Link href="#pricing">Pricing</Nav.Link>
-            </Nav>
-            <Form inline>
-               <FormControl
-                  type="text"
-                  placeholder="Search"
-                  className="mr-sm-2"
-               />
-               <Button variant="outline-primary">Search</Button>
-            </Form>
-         </Navbar>
-      </div>
+      <header>
+         <div id="logo"></div>
+         <div id="countrySerch"></div>
+         <div id="iteamSerch"></div>
+         <div id="icons"></div>
+      </header>
    );
 }
