2ed537fccd22031902165f4a37f38694912a8e7b
Ashan Jameel <59074253+Ashan01@users.noreply.github.com>
2021-02-18 23:32:13 +0500

    commit

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-import Style from "./header.module.css";
+import "./header.module.css";
+import Olx from "../../assets/img/olx.png";
 
 export default function Header() {
    return (
       <header>
-         <div id="logo"></div>
+         <div id="logo">
+            <img src={Olx} height={42} widht={42} alt="logo" />
+         </div>
          <div id="countrySerch"></div>
          <div id="iteamSerch"></div>
          <div id="icons"></div>
