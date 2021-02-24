a685551f0f7d1f38c9f9497dd80949f237728393
Ashan Jameel <59074253+Ashan01@users.noreply.github.com>
2021-02-24 16:12:58 +0500

    Update firebaseConfig.jsx

--- src/Config/firebaseConfig.jsx
+++ src/Config/firebaseConfig.jsx
@@ -1,16 +1,16 @@
-import firebase from "firebase/app";
-import "firebase/database";
-import "firebase/storage";
+// import firebase from "firebase/app";
+// import "firebase/database";
+// import "firebase/storage";
 
-var firebaseConfig = {
-   apiKey: "AIzaSyBZoN9BFPgb4IiLb2IdondvXx3RLYKeppQ",
-   authDomain: "olx-55663.firebaseapp.com",
-   projectId: "olx-55663",
-   storageBucket: "olx-55663.appspot.com",
-   messagingSenderId: "595164694908",
-   appId: "1:595164694908:web:c5951848211c7acf0dbcf9",
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+// var firebaseConfig = {
+//    apiKey: "AIzaSyBZoN9BFPgb4IiLb2IdondvXx3RLYKeppQ",
+//    authDomain: "olx-55663.firebaseapp.com",
+//    projectId: "olx-55663",
+//    storageBucket: "olx-55663.appspot.com",
+//    messagingSenderId: "595164694908",
+//    appId: "1:595164694908:web:c5951848211c7acf0dbcf9",
+// };
+// // Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
 
-export default firebase;
+// export default firebase;
